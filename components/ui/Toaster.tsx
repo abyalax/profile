ceb7b1367c62d3dc13f2892e074ac456a074dadd
abyalax <abyalaxx@gmail.com>
2025-10-12 14:46:35 +0700

    chore: prepare migrate to windows

--- components/ui/Toaster.tsx
+++ components/ui/Toaster.tsx
@@ -1,11 +1,9 @@
-"use client";
+'use client';
 
-import { ToastContainer } from "react-toastify"
+import { ToastContainer } from 'react-toastify';
 
 const Toaster = () => {
-    return (
-        <ToastContainer autoClose={5000} position="top-center" draggable={true}/>
-    )
-}
+  return <ToastContainer autoClose={5000} position="top-center" draggable={true} />;
+};
 
-export default Toaster  
+export default Toaster;
